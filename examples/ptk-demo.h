7451ec42ef2b068c7b7c15f56986e5b870e5e6f5
Guido Günther <agx@sigxcpu.org>
2025-12-21 13:18:58 +0100

    ptk: Add cutout demo
    Renders the cutout information sent by the compositor as bounding boxes. Left click
    maximizes / unmaximizes the surface.
    
    Run e.g. nested with
    
    ```
    PHOC_DEBUG=fake-builtin,cutouts _build/run -E "_build/examples/ptk-demo --run cutouts"
    ```
    
    To add layer-shell to the tests one can spawn an additional client:
    
    ```
    WAYLAND_DISPLAY=wayland-1 _build/examples/layer-shell-effects -l overlay -h 350 -a left -a top -a right -w 0 -f -300 -t 0.5 -e 50 -A
    ```
    
    To overlay different cutouts use e.g.
    
    ```
    export G_RESOURCE_OVERLAYS=/mobi/phosh/gmobile/devices/display-panels=$PWD
    ```
    
    and add a oneplus,fajita.json with the intended data. See
    
    https://phosh.mobi/posts/notch-support/#information-about-the-cutouts
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/734>

--- examples/ptk-demo.h
+++ examples/ptk-demo.h
@@ -10,6 +10,7 @@
 
 G_BEGIN_DECLS
 
+bool                    ptk_demo_cutouts                         (PtkDisplay *display);
 bool                    ptk_demo_subsurface                      (PtkDisplay *display);
 
 G_END_DECLS
