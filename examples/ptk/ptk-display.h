38126829e70c262a9ff10157a21a0f45e37f53ef
Guido Günther <agx@sigxcpu.org>
2025-12-21 13:18:54 +0100

    ptk: Phoc tiny/toy/trivial/toolkit
    Some helpers to create demos more easily
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/740>

---
+++ examples/ptk/ptk-display.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2025 Phosh.mobi e.V.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <glib.h>
+
+#include <wayland-client.h>
+
+G_BEGIN_DECLS
+
+#define PTK_POINTER_EVENT_BUTTON_LEFT 272
+#define PTK_POINTER_EVENT_BUTTON_RIGHT 273
+
+typedef struct _PtkButtonEvent {
+  guint32 button;
+  guint32 state;
+  gint32  x,y;
+} PtkButtonEvent;
+
+
+typedef struct _PtkDisplay {
+  struct wl_display       *display;
+  struct wl_registry      *registry;
+  struct wl_compositor    *compositor;
+  struct wl_subcompositor *subcompositor;
+  struct wl_seat          *seat;
+  struct wl_shm           *shm;
+  struct wl_pointer       *pointer;
+  struct wl_keyboard      *keyboard;
+  struct wl_touch         *touch;
+  struct wl_output        *wl_output;
+  struct xdg_wm_base      *xdg_wm_base;
+} PtkDisplay;
+
+PtkDisplay *            ptk_display_init                        (void);
+PtkDisplay *            ptk_display_get_default                 (void);
+void                    ptk_display_uninit                      (void);
+
+G_END_DECLS
