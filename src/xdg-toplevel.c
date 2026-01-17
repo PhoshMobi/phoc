PhoshMobi/phoc

commit 5b066717bf8aabf87ba0367952e7c297674f3a15

xdg-surfae: Rename source files to xdg-toplevel
Done in a separate commit to ease review.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/675>

src/xdg-toplevel.c
@@ -15,8 +15,8 @@
 #include "server.h"
 #include "view-private.h"
 #include "xdg-popup.h"
-#include "xdg-surface.h"
-#include "xdg-surface-private.h"
+#include "xdg-toplevel.h"
+#include "xdg-toplevel-private.h"
 #include "xdg-toplevel-decoration.h"
 
 #include <wlr/types/wlr_xdg_decoration_v1.h>

src/desktop.c
@@ -51,7 +51,7 @@
 #include "wlr-layer-shell-unstable-v1-protocol.h"
 #include "gesture-swipe.h"
 #include "layer-shell-effects.h"
-#include "xdg-surface.h"
+#include "xdg-toplevel.h"
 #include "xdg-toplevel-decoration.h"
 #include "xwayland-surface.h"
 

src/meson.build
@@ -123,8 +123,8 @@ sources = files(
   'xdg-activation-v1.h',
   'xdg-popup.c',
   'xdg-popup.h',
-  'xdg-surface.c',
-  'xdg-surface.h',
+  'xdg-toplevel.c',
+  'xdg-toplevel.h',
   'xdg-toplevel-decoration.c',
   'xdg-toplevel-decoration.h',
 ) + phoc_anim_sources

src/xdg-toplevel-decoration.c
@@ -1,13 +1,13 @@
 #define G_LOG_DOMAIN "phoc-xdg-toplevel-decoration"
 
 #include "phoc-config.h"
-#include "xdg-surface-private.h"
+#include "xdg-toplevel-private.h"
 #include "xdg-toplevel-decoration.h"
 
 #include <stdbool.h>
 #include <stdlib.h>
 #include <wlr/types/wlr_xdg_shell.h>
-#include "xdg-surface.h"
+#include "xdg-toplevel.h"
 #include "desktop.h"
 #include "server.h"
 #include "utils.h"

src/xdg-toplevel-decoration.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include "xdg-surface.h"
+#include "xdg-toplevel.h"
 
 #include <wlr/types/wlr_compositor.h>
 

src/xdg-toplevel-private.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include "xdg-surface.h"
+#include "xdg-toplevel.h"
 #include "xdg-toplevel-decoration.h"
 
 #include <glib-object.h>

commit dda13d43bcdc08c6a7ab7627683bdc230ca0c397

xdg-toplevel: Fix typo
Fixes: 5b066717b ("xdg-surfae: Rename source files to xdg-toplevel")
Signed-off-by: Arun Mani J <j.arunmani@proton.me>

src/xdg-toplevel.c
@@ -7,7 +7,7 @@
  * Author: Guido Günther <agx@sigxcpu.org>
  */
 
-#define G_LOG_DOMAIN "phoc-xdg-surface"
+#define G_LOG_DOMAIN "phoc-xdg-toplevel"
 
 #include "phoc-config.h"
 