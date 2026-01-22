d846bf134795be9c420e6adec40ecf87bde4e532
Guido Günther <agx@sigxcpu.org>
2026-01-22 17:36:52 +0100

    view: Clear fullscreen output when switching outputs
    When switching from one output to another we should clear the
    old one. We also shouldn't overwrite saved geometry with a
    fullscreen geometry in this case.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/752>

--- src/view.c
+++ src/view.c
@@ -947,10 +947,17 @@ phoc_view_set_fullscreen (PhocView *view, bool fullscreen, PhocOutput *output)
     if (output == NULL)
       output = phoc_view_get_output (view);
 
-    if (was_fullscreen)
-      priv->fullscreen_output->fullscreen_view = NULL;
+    /* Nothing to do if old fullscreen output is the new one */
+    if (was_fullscreen && output == priv->fullscreen_output)
+      return;
 
-    view_save (view);
+    if (was_fullscreen) {
+      /* If switching fullscreen outputs clear the old one */
+      phoc_output_set_fullscreen_view (priv->fullscreen_output, NULL);
+    } else {
+      /* If not yet fullscreen save the non-fullscreen geometry */
+      view_save (view);
+    }
 
     struct wlr_box output_box;
     wlr_output_layout_get_box (desktop->layout, output->wlr_output, &output_box);
