PhoshMobi/phoc

commit d846bf134795be9c420e6adec40ecf87bde4e532

view: Clear fullscreen output when switching outputs
When switching from one output to another we should clear the
old one. We also shouldn't overwrite saved geometry with a
fullscreen geometry in this case.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/752>

src/view.c
@@ -947,10 +947,17 @@ phoc_view_set_fullscreen (PhocView *view, bool fullscreen, PhocOutput *output)
     if (output == NULL)
       output = phoc_view_get_output (view);
 
-    if (was_fullscreen)
-      priv->fullscreen_output->fullscreen_view = NULL;
+    /* Nothing to do if old fullscreen output is the new one */
+    if (was_fullscreen && output == priv->fullscreen_output)
+      return;
 
-    view_save (view);
+    if (was_fullscreen) {
+      /* If switching fullscreen outputs clear the old one */
+      phoc_output_set_fullscreen_view (priv->fullscreen_output, NULL);
+    } else {
+      /* If not yet fullscreen save the non-fullscreen geometry */
+      view_save (view);
+    }
 
     struct wlr_box output_box;
     wlr_output_layout_get_box (desktop->layout, output->wlr_output, &output_box);

commit 14921004ba2f6b59bc94ce9f3ca204b6bca9b5bf

view: Don't exit early if already fullscreen on the output
Different clients want to update geometry later on and currently
we shourtcut it.

Fixes: d846bf134 ("view: Clear fullscreen output when switching outputs")

Closes: https://gitlab.gnome.org/World/Phosh/phoc/-/issues/428
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Tested-by: Teemu Ikonen <tpikonen@mailbox.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/766>

src/view.c
@@ -1002,10 +1002,6 @@ phoc_view_set_fullscreen (PhocView *view, bool fullscreen, PhocOutput *output)
     if (output == NULL)
       output = phoc_view_get_output (view);
 
-    /* Nothing to do if old fullscreen output is the new one */
-    if (was_fullscreen && output == priv->fullscreen_output)
-      return;
-
     if (was_fullscreen) {
       /* If switching fullscreen outputs clear the old one */
       phoc_output_set_fullscreen_view (priv->fullscreen_output, NULL);