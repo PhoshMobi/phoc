PhoshMobi/phoc

commit c1ef0ea8837f5b89527459c3d89f733712719597

output: Use PhocOutputConfig with head states too
This ensures we have a single place that sets pending output state.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/690>

src/output.c
@@ -1843,6 +1843,37 @@ phoc_output_damage_box (PhocOutput *self, const struct wlr_box *box)
 }
 
 
+static PhocOutputConfig *
+phoc_output_config_head_to_output_config (PhocOutput                              *self,
+                                          struct wlr_output_configuration_head_v1 *head)
+{
+  PhocOutputPrivate *priv = phoc_output_get_instance_private (self);
+  PhocOutputConfig *oc = phoc_output_config_new (phoc_output_get_identifier (self));
+
+  oc->enable = TRUE;
+  if (head->state.mode && !head->state.mode->preferred) {
+    oc->mode.width = head->state.mode->width;
+    oc->mode.height = head->state.mode->height;
+    oc->mode.refresh_rate = head->state.mode->refresh / 1000.0;
+  } else {
+    oc->mode.width = head->state.custom_mode.width;
+    oc->mode.height = head->state.custom_mode.height;
+    oc->mode.refresh_rate = head->state.custom_mode.refresh / 1000.0;
+  }
+
+  oc->transform = head->state.transform;
+  oc->scale = adjust_frac_scale (head->state.scale);
+  oc->scale_filter = priv->scale_filter;
+
+  oc->x = head->state.x;
+  oc->y = head->state.y;
+
+  phoc_output_config_dump (oc, "Head state ");
+
+  return oc;
+}
+
+
 static gboolean
 output_manager_apply_config (PhocDesktop                        *desktop,
                              struct wlr_output_configuration_v1 *wlr_config_v1,
@@ -1880,22 +1911,13 @@ output_manager_apply_config (PhocDesktop                        *desktop,
     PhocOutput *output = PHOC_OUTPUT (wlr_output->data);
     struct wlr_output_state pending;
     struct wlr_box output_box;
+    g_autoptr (PhocOutputConfig) oc = NULL;
 
     if (!config_head->state.enabled)
       continue;
 
-    wlr_output_state_init (&pending);
-    wlr_output_state_set_enabled (&pending, true);
-    if (config_head->state.mode != NULL) {
-      wlr_output_state_set_mode (&pending, config_head->state.mode);
-    } else {
-      wlr_output_state_set_custom_mode (&pending,
-                                        config_head->state.custom_mode.width,
-                                        config_head->state.custom_mode.height,
-                                        config_head->state.custom_mode.refresh);
-    }
-    wlr_output_state_set_transform (&pending, config_head->state.transform);
-    wlr_output_state_set_scale (&pending, adjust_frac_scale (config_head->state.scale));
+    oc = phoc_output_config_head_to_output_config (output, config_head);
+    phoc_output_fill_state (output, oc, &pending);
 
     if (test_only) {
       ok &= wlr_output_test_state (wlr_output, &pending);

commit 47099669291208324be9e24a6a9ad0584eb929f8

output: Actually handle preferred mode
The logic was inverted so when we wanted to pick a preferred mode
we ended up in the custom mode path.

This fixes

```sh
$ wlr-randr --output eDP-1 --mode 2880x1920@120
$ wlr-randr --output eDP-1 --mode 2880x1920@60.000999Hz
```

for a laptop with

```
  Modes:
    2880x1920 px, 120.000000 Hz (preferred, current)
    2880x1920 px, 60.000999 Hz (preferred)
```

Also switching refresh rate via g-c-c works again.

Closes: https://gitlab.gnome.org/World/Phosh/phoc/-/issues/429
Fixes: c1ef0ea88 ("output: Use PhocOutputConfig with head states too")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/771>

src/output.c
@@ -1891,7 +1891,7 @@ phoc_output_config_head_to_output_config (PhocOutput                            
   PhocOutputConfig *oc = phoc_output_config_new (phoc_output_get_identifier (self));
 
   oc->enable = TRUE;
-  if (head->state.mode && !head->state.mode->preferred) {
+  if (head->state.mode && head->state.mode->preferred) {
     oc->mode.width = head->state.mode->width;
     oc->mode.height = head->state.mode->height;
     oc->mode.refresh_rate = head->state.mode->refresh / 1000.0;